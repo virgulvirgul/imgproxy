2421533a6718f14fd1f93e27234a0f620c7eda1e
virgulvirgul <trgyspam@gmail.com>
2021-06-20 11:37:57 +0100

    Update landing.go

--- landing.go
+++ landing.go
@@ -5,10 +5,9 @@ import "net/http"
 var landingTmpl = []byte(`
 <!doctype html>
 <html>
-	<head><title>Hey, I'm imgproxy!</title></head>
+	<head><title>image server</title></head>
 	<body>
-		<h1>Hey, I'm imgproxy!</h1>
-		<p style="font-size:1.2em">You can get me here: <a href="https://github.com/imgproxy/imgproxy" target="_blank">https://github.com/imgproxy/imgproxy</a></p>
+		<h1>image server</h1>
 	</body>
 </html>
 `)
